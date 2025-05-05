5c85a7e013b421b6fdf26564c1025d42a40a3996
gregoryb79 <gregoryb79@gmail.com>
2025-05-05 10:21:28 +0300

    spa counters work

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Counter } from "./Counter";
-import { AsyncCounter } from "./AsyncCounter";
 
 // 1. add "SPA navigation" - menu with button to show the synced counters and another button to show the counter list
 // 2. implement "static" counter list - show 4 counters with individual states
@@ -8,23 +7,32 @@ import { AsyncCounter } from "./AsyncCounter";
 //      - show one button: "New counter" - clicking on it will add a counter to the screen
 //      - next to each counter show a delete button to remove the counter from the screen
 //      - counters should not be synced (each should have an individual state)
+// 4. add up/down buttons in each counter in the list to re-order the counters
 // Bonus - maintain the state of the counters when "navigating" back and forth  between the components
 
 function App() {
-  const [selectedView, setSelectedView] = useState<"synced" | "asynced">("synced");
+  const [currentTab, setCurrentTab] = useState<"synced" | "list">("synced");
 
   return (
     <>
-      <nav>
-        <button onClick={() => setSelectedView("synced")}>Synced Counters</button>
-        <button onClick={() => setSelectedView("asynced")}>Counter List</button>
-      </nav>
-      <SyncedCounters />
-      <CounterList />
+      <Nav onSyncedClick={() => setCurrentTab("synced")} onListClick={() => setCurrentTab("list")} />
+      {currentTab === "synced" ? <SyncedCounters /> : <CounterList />}
     </>
   );
 }
 
+type NavProps = {
+  onSyncedClick: () => void;
+  onListClick: () => void;
+};
+function Nav({ onSyncedClick, onListClick }: NavProps) {
+  return (
+    <nav>
+      <button onClick={onSyncedClick}>Synced</button>
+      <button onClick={onListClick}>List</button>
+    </nav>
+  );
+}
 
 function SyncedCounters() {
   const [count, setCount] = useState(0);
@@ -51,7 +59,6 @@ function SyncedCounters() {
 
   return (
     <>
-      <h2>Sync Counters</h2>
       <Counter count={count} onHalfClick={half} onDecreaseClick={decrease} onIncreaseClick={increase} onDoubleClick={double} onResetClick={reset} />
       <Counter count={count} onHalfClick={half} onDecreaseClick={decrease} onIncreaseClick={increase} onDoubleClick={double} onResetClick={reset} />
       <Counter count={count} onHalfClick={half} onDecreaseClick={decrease} onIncreaseClick={increase} onDoubleClick={double} onResetClick={reset} />
@@ -61,10 +68,75 @@ function SyncedCounters() {
 }
 
 function CounterList() {
+  const [counters, setCounters] = useState<number[]>([]);
+
+  function newCounter() {
+    setCounters([...counters, 0]);
+  }
+
+  function half(index: number) {
+    counters[index] = counters[index] / 2;
+    setCounters([...counters]);    
+  }
+
+  function decrease(index: number) {
+    counters[index] = counters[index] - 1;
+    setCounters([...counters]); 
+  }
+
+  function increase(index: number) {
+    counters[index] = counters[index] + 1;
+    setCounters([...counters]); 
+  }
+
+  function double(index: number) {
+    counters[index] = counters[index] * 2;
+    setCounters([...counters]); 
+  }
+
+  function reset(index: number) {
+    counters[index] = 0;
+    setCounters([...counters]); 
+  }
+
+  function deleteCounter(index: number) {
+    const newCounters = [...counters];
+    newCounters.splice(index, 1);
+    setCounters(newCounters);
+  }
+
+  function moveUp(index: number) {    
+    if (index > 0) {
+      const newCounters = [...counters];
+      const temp = newCounters[index - 1];
+      newCounters[index - 1] = newCounters[index];
+      newCounters[index] = temp;
+      setCounters(newCounters);
+    }
+  }
+
+  function moveDown(index: number) {    
+    const newCounters = [...counters];
+    if (index < newCounters.length - 1) {      
+      const temp = newCounters[index + 1];
+      newCounters[index + 1] = newCounters[index];
+      newCounters[index] = temp;
+      setCounters(newCounters);
+    }
+  }
+  
   return (
     <>
-      <h2>Async Counter</h2>
-      <AsyncCounter />
+      <button onClick={newCounter}>New counter</button>
+      <ul>
+        {counters.map((count, index) => 
+          <li key={index}>            
+            <Counter count={count} onHalfClick={() => half(index)} onDecreaseClick={() => decrease(index)} onIncreaseClick={() => increase(index)} onDoubleClick={() => double(index)} onResetClick={() => reset(index)} />          
+            <button onClick={() => deleteCounter(index)}>Delete</button>
+            <button onClick={() => moveUp(index)}>⬆</button>
+            <button onClick={() => moveDown(index)}>⬇</button>
+          </li>)}
+      </ul>
     </>
   );
 }
